2ddbf665f461494c8903d5250475a57c0497e3bf
LichCarlos <1434419455@qq.com>
2025-02-01 20:20:52 +0800

    项目初始化

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+
+import svgr from 'vite-plugin-svgr';
+
+export default defineConfig({
+  plugins: [react(), svgr()],
+});
